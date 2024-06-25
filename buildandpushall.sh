983e34052b146ca2cc4c55fee3a6c18a3dcc1e89
Vincent Prins <vincent.prins@ivdnt.org>
2024-06-25 13:06:06 +0200

    Don't push base images to docker

--- buildandpushall.sh
+++ buildandpushall.sh
@@ -5,11 +5,7 @@ echo "Will build taggers with version <$VERSION_LABEL>. Set VERSION_LABEL to ove
 
 ./buildall.sh
 
-# Base image
-docker push instituutnederlandsetaal/taggers-dockerized-base:$VERSION_LABEL
-
 # PIE
-docker push instituutnederlandsetaal/taggers-dockerized-pie-base:$VERSION_LABEL
 docker push instituutnederlandsetaal/taggers-dockerized-pie-tdn-1400-1600:$VERSION_LABEL
 docker push instituutnederlandsetaal/taggers-dockerized-pie-tdn-1600-1900:$VERSION_LABEL
 docker push instituutnederlandsetaal/taggers-dockerized-pie-tdn-all:$VERSION_LABEL
@@ -20,7 +16,6 @@ docker push instituutnederlandsetaal/taggers-dockerized-pie-tdn-dbnldq:$VERSION_
 
 # Huggingface
 # Commented for now, as we need Git LFS to build these. Perhaps in the future.
-# docker push instituutnederlandsetaal/taggers-dockerized-hug-base:$VERSION_LABEL
 # docker push instituutnederlandsetaal/taggers-dockerized-hug-tdn-1400-1600:$VERSION_LABEL
 # docker push instituutnederlandsetaal/taggers-dockerized-hug-tdn-1600-1900:$VERSION_LABEL
 # docker push instituutnederlandsetaal/taggers-dockerized-hug-tdn-all:$VERSION_LABEL
