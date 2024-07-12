579fe1eca33c9765010ddff76b0f4f32337bb3fb
Vincent Prins <vincent.prins@ivdnt.org>
2024-07-12 16:40:04 +0200

    Moved multithreading implementation to base tagger

--- base/tagger_worker.py
+++ base/tagger_worker.py
@@ -20,6 +20,7 @@ from typing import Optional
 import subprocess
 import requests
 import traceback
+import pathlib
 
 # Local
 import process
@@ -29,6 +30,7 @@ from statuslogger import StatusLogger, ProcessStatus
 from process import PROCESSING_SPEED
 
 CALLBACK_SERVER: str = os.getenv("CALLBACK_SERVER") or ""
+NUM_WORKERS = int(os.getenv("NUM_WORKERS") or 1)
 
 
 def run_pending_tasks() -> None:
@@ -39,7 +41,8 @@ def run_pending_tasks() -> None:
     global pool
 
     # One task at a time.
-    if StatusLogger.busy_task_exists():
+    tasks_in_queue = pool._taskqueue.qsize()
+    if tasks_in_queue > 0:
         return
 
     # Start new task when not busy
@@ -53,11 +56,9 @@ def run_pending_tasks() -> None:
                 # Extra None check for typing
                 if (not is_pool_running(pool)) or pool is None:
                     # Spawn pool if not running
-                    pool = mp.Pool(processes=1, initializer=process.init)
+                    pool = mp.Pool(processes=NUM_WORKERS, initializer=process.init)
                 # Perform task at running pool
                 pool.apply_async(process_file, args=(sl.filename,))
-                # Only start one task at a time, so return.
-                return
 
 
 def process_file(filename: str):
@@ -86,7 +87,7 @@ def process_file(filename: str):
         sl.error(f"An exception occurred: {e}")
         print(traceback.format_exc())
         # copy input file to error folder if it exists
-        if os.path.exists(in_path):
+        if os.path.isfile(in_path):
             sl.error("Moving input file to error folder")
             os.rename(in_path, error_path)
         if CALLBACK_SERVER != "":
@@ -99,20 +100,32 @@ def tag(
 ) -> None:
     """
     Attempt to tag the file by the tagger with a timeout.
-    Send the result to the server, whether sucessful or not.
-    Also appropiately logs the status.
+    Send the result to the server, whether successful or not.
+    Also appropriately logs the status.
     """
     # 300s = 5min fixed time
     # plus
     # bytes * speed variable time
-    in_bytes_size = int(
-        subprocess.check_output(["du", "-sb", in_path]).split()[0].decode("utf-8")
-    )
-    TIMEOUT = 300 + in_bytes_size + PROCESSING_SPEED
-    sl.busy("Will process with a timeout after " + str(TIMEOUT) + " seconds")
+    in_bytes_size = None
+    while in_bytes_size is None:
+        if os.path.isfile(in_path):
+            try:
+                in_bytes_size = int(
+                    subprocess.check_output(["du", "-sb", in_path])
+                    .split()[0]
+                    .decode("utf-8")
+                )
+            except Exception as e:
+                print(f"Error getting file size: {e}")
+                time.sleep(1)
+        else:
+            raise FileNotFoundError(f"File {in_path} not found")
 
-    # Runs the respective tagger software.
-    @timeout(TIMEOUT, os.strerror(errno.ETIME))
+    time_out = 300 + in_bytes_size + PROCESSING_SPEED
+    sl.busy("Will process with a timeout after " + str(time_out) + " seconds")
+
+    # Runs the respective tagger software synchronously.
+    @timeout(time_out, os.strerror(errno.ETIME))
     def doTagging():
         process.process(in_path, out_path)
 
@@ -121,11 +134,7 @@ def tag(
     # Done processing
     ps.delete_status()  # Frees up the tagger
     sl.finished("Removing input file")
-    # "try", because the task might have been cancelled and deleted in the meantime.
-    try:
-        os.remove(in_path)
-    except:
-        pass
+    pathlib.Path(in_path).unlink(missing_ok=True)
 
     sl.finished(
         "Finished processing %s, result has size %d"
@@ -190,12 +199,15 @@ def is_pool_running(pool: Optional[Pool]) -> bool:
 
 # Pool needs to be defined after the functions it will execute.
 # https://stackoverflow.com/questions/41385708/multiprocessing-example-giving-attributeerror#comment101561695_42383397
-pool: Optional[Pool] = mp.Pool(processes=1, initializer=process.init)
+pool = None
 
-
-# It is ugly, but it is also used here:
-# https://pypi.org/project/schedule/
 if __name__ == "__main__":
+    # Can't use fork with the gpu.
+    mp.set_start_method("spawn", force=True)
+    pool = mp.Pool(processes=NUM_WORKERS, initializer=process.init)
+
+    # It is ugly, but it is also used here:
+    # https://pypi.org/project/schedule/
     while True:
         run_pending_tasks()
-        time.sleep(1)
+        time.sleep(0.05)
