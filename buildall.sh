03845e3cc5f72314c3fdf1e479435d0c0dbb6182
Vincent Prins <vincent.prins@ivdnt.org>
2024-10-01 13:34:56 +0200

    Add flair to buildall

--- buildall.sh
+++ buildall.sh
@@ -9,3 +9,4 @@ docker build --build-arg VERSION=$VERSION -t instituutnederlandsetaal/taggers-do
 # ud-parsers
 docker build --build-arg VERSION=$VERSION --build-arg SPACY_MODEL=nl_core_news_lg -t instituutnederlandsetaal/taggers-dockerized-spacy:$VERSION spacy
 docker build --build-arg VERSION=$VERSION -t instituutnederlandsetaal/taggers-dockerized-stanza:$VERSION stanza
+docker build --build-arg VERSION=$VERSION -t instituutnederlandsetaal/taggers-dockerized-flair:$VERSION flair
